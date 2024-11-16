2c6bf652be3a34c7d7638d7bb7a8d29638bd6078
aknlite48 <aanandk2705@gmail.com>
2024-11-15 21:16:46 -0500

    added resp_denom for E step

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ Dataset = [*,*]x(N,D)   | access: i*(D) + j
 //kernels
 __global__ void matrixInverse(float *matrices, float *inverses, float *determinants, int n);
 __global__ void gauss_calc(float* d_gaussians,float* data,float* u_k,float* E_k_inv,float* E_k_det,int N,int K, int D);
+__global__ void E_step(float* d_gaussians,float* pi_k,float* resp,int N, int K);
 
 //__global__ void E_step_denom()
 
@@ -46,6 +47,8 @@ int main() {
 	float E_k[(K*D*D)];
 	float data[(N*D)];
 
+	float log_LL=0;
+
 	read_data(data,"data.csv");
 	read_data(pi_k,"weights.csv");
 	read_data(u_k,"means.csv");
@@ -60,6 +63,9 @@ int main() {
 	float* d_E_k_det;
 
 	float* d_gaussians;
+	float* d_resp;
+	float* d_resp_denom;
+	float* d_log_LL;
 
 	//cuda alloc
 	int alloc_size1 = K*sizeof(float);
@@ -74,15 +80,27 @@ int main() {
 	cudaMalloc((void**)&d_E_k_inv,alloc_size3);
 	cudaMalloc((void**)&d_E_k_det,alloc_size1);
 
+	cudaMalloc((void**)&d_gaussians,N*K*sizeof(float));
+	cudaMalloc((void**)&d_resp,N*K*sizeof(float));
+	cudaMalloc((void**)&d_resp_denom,N*sizeof(float));
+	cudaMalloc((void**)&d_log_LL,sizeof(float));
+
+
 	//cuda memcpy
 	cudaMemcpy(d_pi_k,pi_k,alloc_size1,cudaMemcpyHostToDevice);
 	cudaMemcpy(d_u_k,u_k,alloc_size2,cudaMemcpyHostToDevice);
 	cudaMemcpy(d_E_k,E_k,alloc_size3,cudaMemcpyHostToDevice);
 
+	cudaMemcpy(d_log_LL,&log_LL,sizeof(float),cudaMemcpyHostToDevice);
+
 
 	//kernel invocation
 	//calculate E matrix inverse and determinant
-	matrixInverse<<<K,1>>>(d_E_k,d_E_k_inv,d_E_k_det,N);
+	matrixInverse<<<K,1>>>(d_E_k,d_E_k_inv,d_E_k_det,D);
+
+	//M Step : calc gaussians & update responsibilities
+	gauss_calc<<<>>>
+	M_step<<<>>>
 
 	//calculate gaussians 
 
@@ -139,20 +157,55 @@ __global__ void matrixInverse(float *matrices, float *inverses, float *determina
     determinants[idx] = determinant; // Store the determinant
 }
 
-/*
+
 __global__ void gauss_calc(float* d_gaussians,float* data,float* u_k,float* E_k_inv,float* E_k_det,int N,int K,int D) {
 
-	int tid =;
+	int tid0 = (blockIdx.x*blockDim.x)+threadIdx.x;
+	int tot_threads = gridDim.x*blockDim.x;
 	int size = N*K;
-	if (tid<size) {
+	for (int tid=tid0;tid<size;tid+=tot_threads) {
 		int x_n = tid/K; //xn = d_data[(x_n*D):(x_n)]
 		int k_n = tid%K;
 
+		float x_minus_u[dim];
+		for (int i=0;i<dim;i++) {
+			x_minus_u[i] = d_data[x_n*D+i]-u_k[k_n*D+i];
+		}
 
-		d_gaussians[tid] = gaussian();
+		float final_val=0; float temp_val=0;
+
+		for (int i=0;i<dim;i++) {
+			temp_val=0;
+			for (int j=0;j<dim;j++) {
+				temp_val+=E_k_inv[(D*D*k_n)+(D*i)+j]*x_minus_u[j];
+			}
+			final_val+=x_minus_u[i]*temp_val;
+		}
+		d_gaussians[tid] = final_val;
 	}
-
-
-
 }
-*/
+
+__global__ void resp_denom_update(float* gaussians,float* pi_k,float* resp_denom,float* log_LL,int N,int K) {
+	int tid0 = (blockIdx.x*blockDim.x)+threadIdx.x;
+	int size = N;
+	if (tid0<size) {
+		float sum=0;
+		for (int i=0;i<K;i++) {
+			sum+=gaussians[(tid0*K)+i]*pi_k[i];
+		}
+		resp_denom[tid0]=sum;
+		log_LL+=log(sum);
+	}
+}
+
+__global__ void E_step(float* gaussians,float* pi_k,float* resp,float* resp_denom,int N,int K) {
+	int tid0 = (blockIdx.x*blockDim.x)+threadIdx.x;
+	int tot_threads = gridDim.x*blockDim.x;
+	int size = N*K;
+	for (int tid=tid0;tid<size;tid+=tot_threads)
+		int x_n = tid/K; //xn = d_data[(x_n*D):(x_n)]
+		int k_n = tid%K;
+		resp[tid] = gaussians[tid]/resp_denom[x_n];
+}
+
+
